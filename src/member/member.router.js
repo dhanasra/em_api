9422bff668fa1e5dc3846aecf088aae78a714dc8
dhanasekaran <dhana.spiderlingz@gmail.com>
2022-10-11 00:13:18 +0530

    update cashbook get

--- src/member/member.router.js
+++ src/member/member.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { verifyToken } = require('../auth/auth.controller');
 const { create, update, details,delete:memberDelete } = require('./member.controller');
 
 const router = express.Router();
 
-router.post('CB/:id', create);
-router.get('/:id', details);
-router.put('/cashbook/:id',update);
-router.delete('/cashbook/:id/member/:memberId',memberDelete);
+router.post('/CB/:id', verifyToken, create);
+router.get('/:id', verifyToken, details);
+router.put('/cashbook/:id', verifyToken, update);
+router.delete('/cashbook/:id/member/:memberId', verifyToken, memberDelete);
 
 module.exports.memberRouter = router
